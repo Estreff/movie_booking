ef10f648c5c57839910ae235ad6ef2568bb6d789
Estreff <estreff@gmail.com>
2021-03-25 14:51:04 -0600

    Initial Push

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 const container = document.querySelector('.container');
+const allSeats = document.querySelectorAll('.row .seat');
 const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const book = document.getElementById('book_seats');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
+const movieIndex = document.getElementById('movie').selectedIndex;
 
 populateUI();
 
 let ticketPrice = +movieSelect.value;
+console.log('Ticket Price: ', ticketPrice);
 
 // Save selected movie index and price
-function setMovieData(movieIndex, moviePrice) {
-  localStorage.setItem('selectedMovieIndex', movieIndex);
-  localStorage.setItem('selectedMoviePrice', moviePrice);
+function setMovieData(movieIndex, ticketPrice, movieSeats) {
+  const movieStorage = {
+    movieIndex: movieIndex,
+    ticketPrice: ticketPrice,
+    occupiedSeats: movieSeats
+  }
+  console.log(movieIndex);
+  localStorage.setItem(`${movieIndex}`, JSON.stringify(movieStorage));
 }
 
 // Update total and count
@@ -24,8 +33,26 @@ function updateSelectedCount() {
   
   const selectedSeatsCount = selectedSeats.length;
 
+  console.log('Movie Price: ', ticketPrice);
+
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// Update total booked
+function updateBookedCount() {
+  const bookedSeats = document.querySelectorAll('.row .seat.selected');
+
+  bookedSeats.forEach(seat => {
+    seat.classList.add('occupied');
+    seat.classList.remove('selected');
+  })
+  updateSelectedCount();
+
+  const seatsIndex = [...bookedSeats].map(seat => [...seats].indexOf(seat));
+  console.log(movieIndex, ticketPrice, seatsIndex);
+
+  setMovieData(movieIndex, ticketPrice, seatsIndex);
 }
 
 // Get data from localstorage and populate UI
@@ -48,11 +75,55 @@ function populateUI() {
 }
 
 // Movie select event
-movieSelect.addEventListener('change', e => {
-  ticketPrice = +e.target.value;
-  setMovieData(e.target.selectedIndex, e.target.value);
-  updateSelectedCount();
-})
+// movieSelect.addEventListener('change', e => {
+//   console.log('Movie Index: ', e.target.dataset.movieIndex);
+//   if (e.target.value == 0) {
+//     allSeats.forEach(seat => {
+//       seat.classList.remove('occupied');
+//       seat.classList.remove('selected');
+//       seat.classList.add('disabled');
+//     })
+//   } else {
+//     allSeats.forEach(seat => {
+//       seat.classList.remove('disabled');
+//     })
+//   }
+
+//   ticketPrice = +e.target.value;
+//   // setMovieData(e.target.selectedIndex, e.target.value);
+//   updateSelectedCount();
+// })
+
+function selectMovie(menu) {
+  const selectedMovie = parseInt(menu.options[menu.selectedIndex].getAttribute('data-movie'));
+  const selectedMoviePrice = menu.options[menu.selectedIndex].value;
+
+  console.log('Selected Movie: ', selectedMovie);
+
+  const getMovie = localStorage.getItem(`${selectedMovie}`);
+  if(selectedMovie === 0) {
+    console.log('Select Movie');
+    allSeats.forEach(seat => {
+      seat.classList.remove('occupied');
+      seat.classList.remove('selected');
+      seat.classList.add('disabled');
+    })
+  } else if(getMovie !== null) {
+    allSeats.forEach(seat => {
+      seat.classList.remove('disabled');
+    })
+
+    const occupied = getMovie.occupiedSeats;
+    const movieInfo = {
+      movieIndex: selectedMovie,
+      moviePrice: selectedMoviePrice
+    }
+  
+    console.log('Movie Info: ', movieInfo);
+  }  else {
+    console.log(`This movie doesn't have info`);
+  }
+}
 
 
 // Seat click event
@@ -64,6 +135,11 @@ container.addEventListener('click', e => {
       e.target.classList.toggle('selected');
       updateSelectedCount();
   }
+})
+
+// Book the seats
+book.addEventListener('click', e => {
+  updateBookedCount();
 })
 
 // Initial count and total set
---
+++ script_test.js
@@ -0,0 +1,137 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat');
+const book = document.getElementById('book_seats');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+
+// Populate UI
+disableSeats();
+  
+
+// Update total and count
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  const ticketPrice = +movieSelect.value;
+  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+  const selectedSeatsCount = selectedSeats.length;
+
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// Update total booked
+function updateBookedCount() {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  // const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+  // const bookedSeats = document.querySelectorAll('.row .seat.occupied');
+  // const occupiedIndex = [...bookedSeats].map(seat => [...seats].indexOf(seat));
+  selectedSeats.forEach(seat => {
+    seat.classList.add('occupied');
+    seat.classList.remove('selected');
+  })
+  updateSelectedCount();
+  
+  setMovieData(getMovieIndex(), getTicketPrice(), getOccupiedSeats(), getSelectedSeats());
+}
+
+function selectMovie(menu) {
+  const selectedMovie = menu.options[menu.selectedIndex].getAttribute('data-movie');
+  const selectedMoviePrice = menu.options[menu.selectedIndex].value;
+
+  const getMovie = JSON.parse(localStorage.getItem(`${selectedMovie}`));
+  if(selectedMovie == 0) {
+    disableSeats();
+    
+  } else if(getMovie !== null) {
+    const reserved = getMovie.reservedSeats;
+    const occupied = getMovie.occupiedSeats;
+    clearSeats();
+    seats.forEach((seat, index) => {
+      if(reserved.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+      if(occupied.indexOf(index) > -1) {
+        seat.classList.add('occupied');
+      }
+    })
+  }  else {
+    clearSeats();
+  }
+}
+
+
+// Seat click event
+container.addEventListener('click', e => {
+  if(
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
+    ) {
+      e.target.classList.toggle('selected');
+      setMovieData(getMovieIndex(), getTicketPrice(), getOccupiedSeats(), getSelectedSeats());
+      updateSelectedCount();
+  }
+})
+
+// Book the seats
+book.addEventListener('click', e => {
+  updateBookedCount();
+})
+
+// Get Movie Index
+function getMovieIndex() {
+  return movieSelect.options[movieSelect.selectedIndex].getAttribute('data-movie');
+}
+
+// Get Ticket Price
+function getTicketPrice() {
+  return movieSelect.value;
+}
+
+// Clear seating
+function clearSeats() {
+  return seats.forEach(seat => {
+    seat.classList.remove('occupied');
+    seat.classList.remove('selected');
+    seat.classList.remove('disabled');
+  })
+}
+
+function disableSeats() {
+  return seats.forEach(seat => {
+    seat.classList.remove('occupied');
+    seat.classList.remove('selected');
+    seat.classList.add('disabled');
+  })
+}
+
+function getSelectedSeats() {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  return [...selectedSeats].map(seat => [...seats].indexOf(seat));
+}
+
+function getOccupiedSeats() {
+  const bookedSeats = document.querySelectorAll('.row .seat.occupied');
+  return [...bookedSeats].map(seat => [...seats].indexOf(seat));
+}
+
+// Save selected movie index and price
+function setMovieData(movieIndex, ticketPrice, occupiedSeats, selectedSeats) {
+  const movieStorage = {
+    movieIndex: movieIndex,
+    ticketPrice: ticketPrice,
+    occupiedSeats: occupiedSeats,
+    reservedSeats: selectedSeats
+  }
+  localStorage.setItem(`${movieIndex}`, JSON.stringify(movieStorage));
+}
+
+function resetMovie() {
+  seats.forEach(seat => {
+    seat.classList.remove('occupied');
+    seat.classList.remove('selected');
+  })
+
+  setMovieData(getMovieIndex(), getTicketPrice(), [], []);
+}
